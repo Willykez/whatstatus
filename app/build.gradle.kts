3b40b240ef66df01c85d6b5a14512506cfa01dbc
Repo Master User <repomaster@users.noreply.github.com>
2026-08-21 05:30:03 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
   namespace = "com.willykez.wastatus"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.willykez.wastatus"
     minSdk = 24
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
   }
